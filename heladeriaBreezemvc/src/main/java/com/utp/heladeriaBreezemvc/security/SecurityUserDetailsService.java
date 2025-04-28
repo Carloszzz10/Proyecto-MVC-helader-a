b35dd1d60cb46feecdbb01d2c1d7f6b4d43398d6
benjamin31-05 <benjaminestebann31@gmail.com>
2025-04-28 01:40:31 -0500

    feat(security): añadir clases SecurityUserDetails y SecurityUserDetailsService para gestión de usuarios en la autenticación

---
+++ heladeriaBreezemvc/src/main/java/com/utp/heladeriaBreezemvc/security/SecurityUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.utp.heladeriaBreezemvc.security;
+
+import com.utp.heladeriaBreezemvc.model.Usuario;
+import com.utp.heladeriaBreezemvc.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+            
+        return new SecurityUserDetails(usuario);
+    }
+}
